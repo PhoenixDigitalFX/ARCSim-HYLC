e20d514dd9af9a4e7dbfb88b8f80a0b73d30f445
Georg <gsperl@ldchwo009.ist.local>
2019-03-15 13:17:57 +0100

    mathematica pass by value

--- v0.2.1/hylc/MathematicaDefinitions.h
+++ v0.2.1/hylc/MathematicaDefinitions.h
@@ -7,11 +7,10 @@ namespace hylc {
 namespace mathematica {
 
 typedef double Real;
-inline Real Power(const Real &A, const Real &B) { return std::pow(A, B); }
+inline Real Power(Real A, Real B) { return std::pow(A, B); }
 // inline Real Power(const Real &A, const int &B) { return std::pow(A, B); }
-inline Real Power(const Real &A, const int &B) {
-  if (abs(B) > 8) // arbitary threshold
-    return std::pow(A, B);
+inline Real Power(Real A, int B) {
+  assert(abs(B) < 8 && "require higher pow, implement using std.");
   Real out = 1.0;
   if (B >= 0) {
     for (int i = 0; i < B; ++i)
@@ -23,15 +22,16 @@ inline Real Power(const Real &A, const int &B) {
   }
   return out;
 }
-inline Real Abs(const Real &A) { return std::abs(A); }
-inline Real Sqrt(const Real &A) { return std::sqrt(A); }
-inline Real Exp(const Real &A) { return std::exp(A); }
-inline Real Sin(const Real &A) { return std::sin(A); }
-inline Real Cos(const Real &A) { return std::cos(A); }
-inline Real ArcCos(const Real &A) { return std::acos(A); }
-inline Real ArcTan(const Real &X, const Real &Y) { return std::atan2(Y, X); }
-inline Real Tan(const Real &X) { return std::tan(X); }
-inline Real Sec(const Real &X) { return 1.0/std::cos(X); }
+inline Real Abs(Real A) { return std::abs(A); }
+inline Real Sqrt(Real A) { return std::sqrt(A); }
+inline Real Exp(Real A) { return std::exp(A); }
+inline Real Sin(Real A) { return std::sin(A); }
+inline Real Cos(Real A) { return std::cos(A); }
+inline Real Log(Real A) { return std::log(A); }
+inline Real ArcCos(Real A) { return std::acos(A); }
+inline Real ArcTan(Real X, const Real Y) { return std::atan2(Y, X); }
+inline Real Tan(Real X) { return std::tan(X); }
+inline Real Sec(Real X) { return 1.0/std::cos(X); }
 
 } // namespace Mathematica
 } // namespace HYLC
