19438d672d8cdcaba1981451c31cc167c09dfd4a
Georg SPERL <gsperl@ldchwo009.ist.local>
2019-12-04 10:36:31 +0100

    coarsen 2D and move folder

--- replay_offline.sh
+++
@@ -1,7 +0,0 @@
-#called from within build dir
-
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-  && make -j arcsim_0.2.1 \
-  && pushd .. \
-  && ./build-Release/bin/arcsim_0.2.1 replay ${1} \
-  && popd
---
+++ run_folder.py
@@ -0,0 +1,194 @@
+"""
+This script runs a folder full of sims expecting/creating the following structure:
+  folder/conf/*.json    ..  input
+  folder/sims/          ..  output
+"""
+import platform
+assert(platform.python_version().startswith("3")) # cluster safety
+import os, sys, subprocess, argparse, time
+from multiprocessing import Pool
+import getpass, smtplib, signal
+from email.mime.text import MIMEText
+
+# HARDCODED ###################################################################
+email_user = 'gsperl'
+email_server = 'owa.ist.ac.at'
+
+
+# ARGS ########################################################################
+
+ap = argparse.ArgumentParser()
+ap.add_argument('folder', help='input folder with conf/ subfolder')
+
+ap.add_argument("-b", "--build", default="1",
+                help="...")
+ap.add_argument("-r", "--run", default="1",
+                help="...")
+ap.add_argument("-o", "--output", default="sims",
+                help="...")
+ap.add_argument("-p", "--processes", default=1, type=int,
+                help="...")
+ap.add_argument("-d", "--delay", default=0, type=int,
+                help="delay subprocess by n seconds to avoid initial remeshing clash")
+ap.add_argument("-e", "--email", action='store_true')
+ap.add_argument("-f", "--filter", default=[], nargs="*",
+                help="filter to include")
+ap.add_argument("-F", "--filterex", default=[], nargs="*",
+                help="filter to exclude")
+
+args, unknownargs = ap.parse_known_args()
+args = vars(args)
+args['build'] = args['build'] != "0" and args['build'].lower() != "false"
+args['run'] = args['run'] != "0" and args['run'].lower() != "false"
+
+
+# HELPER FUNCS ################################################################
+
+def try_timeout(func, T=60, default=None):
+    signal.signal(signal.SIGALRM, lambda signum, frame: 1/0)
+    signal.alarm(T)
+    ret = default
+    try:
+        ret = func()
+    except ZeroDivisionError:
+        print("Timed out.")
+        ret = default
+    except:
+        print("Aborted.")
+        ret = default
+    finally:
+        signal.alarm(0)
+    return ret
+
+try_getmsg = lambda: try_timeout(lambda: input("Enter e-mail message: "))
+try_getpass = lambda: try_timeout(lambda: getpass.getpass("Enter e-mail password: "))
+
+def try_send_mail(header, content, server, user, pw):
+    usermail = '%s@ist.ac.at' % user
+    msg = MIMEText(content)
+    msg['Subject'] = header
+    msg['From'] = usermail
+    msg['To'] = usermail
+
+    try:
+        server = smtplib.SMTP(server, 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pw)
+        server.send_message(msg)
+        print("Sent email.")
+        server.close()
+    except:
+        print("Email error.")
+
+def build(sourcedir, builddir, debug=False):
+    cfg = 'Debug' if debug else 'Release'
+    cmake_args = ['-DCMAKE_BUILD_TYPE=' + cfg]
+    build_args = ['--config', cfg]
+    build_args += ['--', '-j']
+
+    os.makedirs(builddir, exist_ok=True)
+
+    subprocess.check_call(['cmake', sourcedir] +
+                          cmake_args, cwd=builddir)
+    subprocess.check_call(['cmake', '--build', '.'] +
+                          build_args, cwd=builddir)  # basically make
+
+# BUILD #######################################################################
+
+sourcedir = "."
+builddir = os.path.join(sourcedir, "build-Release")
+
+if args['build']:
+    build(sourcedir=sourcedir, builddir=builddir, debug=False)
+
+if not args['run']:
+    exit()
+
+if args['email']:
+    email_pw = try_getpass()
+    if email_pw is None or email_pw == "":
+        args['email'] = False
+    else:
+        email_pw = email_pw[::-1]
+        email_msg = try_getmsg()
+        if email_msg is None:
+            email_msg = ""
+
+# RUN #########################################################################
+
+workdir = sourcedir
+executable = os.path.join(builddir, "bin", "arcsim_0.2.1")
+# from workdir call: ./build-Release/bin/arcsim_0.2.1 $op $configfile ${3}
+
+op = "simulateoffline"
+
+confs = sorted(os.listdir(os.path.join(args['folder'], "conf")))
+os.makedirs(os.path.join(args['folder'],args['output']), exist_ok=True)
+
+def tasks():
+    delay = 0
+    i = 0
+    for conf in confs:
+        if not conf.endswith(".json"):
+            continue
+
+        # exclusive filter
+        if any([fil in conf for fil in args['filterex']]):
+            continue
+        # inclusive filter
+        if not all([fil in conf for fil in args['filter']]):
+            continue
+
+        simname = os.path.splitext(os.path.basename(conf))[0]
+        confpath = os.path.join(args['folder'], "conf", conf) 
+        outputdir = os.path.join(args['folder'], args['output'], simname) 
+        if os.path.isdir(outputdir):
+            print("Skipping existing/in-progress", conf)
+            continue
+
+        yield (i,[executable, op, confpath, outputdir], delay)
+        delay += args['delay']
+        i += 1
+
+def execute_task(args):
+    i,task,delay = args
+    outputdir = task[-1]
+    if os.path.isdir(outputdir): # try skip again, if tasks preallocated then overlaps can happen..?
+        print("Skipping existing/in-progress", task[-2])
+        return
+    if delay > 0:
+        print("Delaying task %d for %02dm %02ds" % (i, delay//60, delay%60))
+        time.sleep(delay)
+    print("Starting task", i)
+    subprocess.check_call(task, cwd=workdir)
+
+try:
+    n_processes = int(args['processes'])
+    if n_processes > 1:
+        pool = Pool(n_processes)
+        iterator = tasks()
+        pool_it = pool.imap_unordered(execute_task, iterator, chunksize=1)
+        # wait and iterate pool results, keyboard abortable
+        try:
+            n_finished = 0
+            for _ in pool_it:
+                n_finished += 1
+                print("Finished simulation #%d." % n_finished)
+        except KeyboardInterrupt:
+            print("Caught KeyboardInterrupt, terminating workers")
+            pool.terminate() # terminating 
+        else:
+            pool.close() # normal termination
+    else:
+        for task in tasks():
+            execute_task(task)
+    
+except KeyboardInterrupt:
+    print("PY: Aborting execution")
+
+    if args['email']:
+        print("Email message was:\n", email_msg)
+else:
+    if args['email']:
+        try_send_mail("HYLC Finished", "The simulations have finished.\n" + email_msg, email_server, email_user, email_pw[::-1])
